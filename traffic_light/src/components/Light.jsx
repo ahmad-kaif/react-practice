ae93a54baa0978a07eec0caf812680c6bcca4b11
Ahmad Kaif <heyahmad611@gmail.com>
2025-01-13 15:18:48 +0530

    logic done

--- traffic_light/src/components/Light.jsx
+++ traffic_light/src/components/Light.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Light = () => {
+  const [result, setResult] = useState("Go!!");
+  const [currentLight, setCurrentLight] = useState("green"); // Tracks the current light (green, yellow, red)
+
+
+  useEffect(() => {
+    const lightSeq = {
+      green: { next: "yellow", message: "Slow Down", duration: 4000 },
+      yellow: { next: "red", message: "Stop!!", duration: 3000 },
+      red: { next: "green", message: "Go!!", duration: 4000 },
+    };
+
+    const timer = setTimeout(() => {
+      const nextLight = lightSeq[currentLight];
+      setCurrentLight(nextLight.next);
+      setResult(nextLight.message);
+    }, lightSeq[currentLight].duration);
+
+  }, [currentLight]);
+
+   // Light color classes based on state
+   const lightStyles = {
+    red: currentLight === "red" ? "bg-red-600" : "bg-red-100",
+    yellow: currentLight === "yellow" ? "bg-yellow-400" : "bg-yellow-100",
+    green: currentLight === "green" ? "bg-green-600" : "bg-green-100",
+  };
+
   return (
-    <div className='h-screen bg-black text-white'>
-    
-      <div className="flex items-center justify-center border border-white h-56 ">
-            <div className="border border-yellow-500">
-                <div className=" border rounded-full h-11 w-12 bg-red-600 border-r m-3 flex justify-center items-center">R</div>
-                <div className="border rounded-full h-11 bg-yellow-500 border-r m-3 flex justify-center items-center">Y</div>
-                <div className="border rounded-full h-11 bg-green-500 border-r m-3 flex justify-center items-center">G</div>
-            </div>
+    <div className="h-screen bg-black text-white">
+      <div className="flex flex-col items-center justify-center h-72 ">
+        <div className="border border-yellow-500">
+          <div  className={`text-black border rounded-full h-11 w-12 m-3 flex justify-center items-center ${lightStyles.red}`}>
+            R
+          </div>
+          <div className={`text-black border rounded-full h-11 w-12 m-3 flex justify-center items-center ${lightStyles.yellow}`}>
+            Y
+          </div>
+          <div  className={`text-black border rounded-full h-11 w-12 m-3 flex justify-center items-center ${lightStyles.green}`}>
+            G
+          </div>
+        </div>
+        <div className="mt-3 text-3xl font-bold">
+          {result}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Light
+export default Light;
